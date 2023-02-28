07ab4a06861b7f0a0e055ab8dc775ff809a65bea
christiandarvs <christiandarvin4@gmail.com>
2023-02-28 16:13:49 +0800

    add files

---
+++ lib/feb_twenty_eight.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Feb28 extends StatelessWidget {
+  const Feb28({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:daily_study/feb_twenty_eight.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      title: 'Material App',
+      home: const Feb28(),
+    );
+  }
+}
