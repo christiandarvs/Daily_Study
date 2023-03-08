396179806bf4a7fe0f4bf124c2930ff1e2833e95
christiandarvs <christiandarvin4@gmail.com>
2023-03-08 23:59:25 +0800

    add march 8 study - badges

--- lib/march_eight.dart
+++ lib/march_eight.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:badges/badges.dart';
+
+int basketCount = 0;
 
 class March8 extends StatefulWidget {
   const March8({super.key});
@@ -8,86 +11,220 @@ class March8 extends StatefulWidget {
 }
 
 class _March8State extends State<March8> {
-  List<String> programmingLanguages = [
-    'Java',
-    'Dart',
-    'Kotlin',
-    'Swift',
-    'Python',
-    'Javascript',
-    'C',
-    'C++',
-    'C#',
-    'Go',
-    'Rust',
-  ];
-  List<bool> checkBoxValues = [
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false
-  ];
-  void showCompletionPrompt(BuildContext context) {
-    showDialog(
-        context: context,
-        builder: ((context) {
-          return AlertDialog(
-            title: Row(
-              children: const [
-                Icon(
-                  Icons.celebration,
-                  color: Colors.orange,
-                ),
-                SizedBox(
-                  width: 15,
-                ),
-                Text('Well done!'),
-              ],
-            ),
-            content: const Text('You did great today!'),
-            actions: [
-              TextButton(
-                  onPressed: (() {
-                    Navigator.pop(context);
-                  }),
-                  child: const Text('OK'))
-            ],
-          );
-        }));
-  }
-
+  int notifNumber = 0;
+  int followerRequests = 10;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Shopee'),
+        backgroundColor: Colors.orange.shade700,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: IconButton(
+                onPressed: (() {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: ((context) {
+                    return SecondPage(
+                      itemQuantity: basketCount,
+                    );
+                  })));
+                }),
+                icon: Badge(
+                  position: BadgePosition.custom(start: 15, bottom: 5),
+                  showBadge: basketCount > 0 ? true : false,
+                  badgeStyle: const BadgeStyle(
+                    badgeColor: Colors.red,
+                  ),
+                  badgeAnimation: const BadgeAnimation.size(toAnimate: false),
+                  badgeContent: Text(
+                    '$basketCount',
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                  child: const Icon(
+                    Icons.shopping_bag_outlined,
+                    size: 25,
+                  ),
+                )),
+          ),
+        ],
+      ),
       body: SafeArea(
-          child: ListView.builder(
-              itemCount: programmingLanguages.length,
-              itemBuilder: ((context, index) {
-                return CheckboxListTile(
-                    selected: checkBoxValues[index],
-                    activeColor: Colors.green,
-                    subtitle: const Text('Programming Language'),
-                    secondary: const Icon(Icons.code),
-                    title: Text(programmingLanguages[index]),
-                    value: checkBoxValues[index],
-                    onChanged: ((value) {
-                      setState(() {
-                        checkBoxValues[index] = value!;
-                        var checkIfDone =
-                            checkBoxValues.every((element) => element == true);
-                        if (checkIfDone) {
-                          showCompletionPrompt(context);
-                        }
-                      });
-                    }));
-              }))),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 200,
+                width: 200,
+                child: Image.network(
+                    'https://cf.shopee.ph/file/4e9b29f378bdeb72bf313429e2d2223d'),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                      onPressed: (() {
+                        setState(() {
+                          basketCount--;
+                        });
+                      }),
+                      child: const Text(
+                        'Remove',
+                        style: TextStyle(color: Colors.red),
+                      )),
+                  TextButton(
+                      onPressed: (() {
+                        setState(() {
+                          basketCount++;
+                        });
+                      }),
+                      child: const Text(
+                        'Add Item',
+                        style: TextStyle(),
+                      ))
+                ],
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Badge(
+                badgeStyle: const BadgeStyle(
+                  badgeColor: Colors.blue,
+                ),
+                badgeContent: Text(
+                  '$notifNumber',
+                  style: const TextStyle(color: Colors.white),
+                ),
+                badgeAnimation: const BadgeAnimation.scale(
+                    curve: Curves.easeInOut,
+                    toAnimate: false,
+                    animationDuration: Duration(milliseconds: 250)),
+                showBadge: notifNumber > 0 ? true : false,
+                child: const Icon(
+                  Icons.shopping_cart_outlined,
+                  size: 70,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                      onPressed: (() {
+                        setState(() {
+                          notifNumber--;
+                        });
+                      }),
+                      child: const Text(
+                        'Remove',
+                        style: TextStyle(color: Colors.red),
+                      )),
+                  TextButton(
+                      onPressed: (() {
+                        setState(() {
+                          notifNumber++;
+                        });
+                      }),
+                      child: const Text(
+                        'Add Item',
+                        style: TextStyle(),
+                      ))
+                ],
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Badge(
+                showBadge: followerRequests > 0 ? true : false,
+                badgeContent: Text(
+                  '$followerRequests',
+                  style: const TextStyle(color: Colors.white),
+                ),
+                badgeAnimation: const BadgeAnimation.scale(
+                    curve: Curves.easeInOut,
+                    animationDuration: Duration(milliseconds: 250)),
+                child: const Icon(
+                  Icons.person,
+                  size: 50,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                      onPressed: (() {
+                        setState(() {
+                          followerRequests--;
+                        });
+                      }),
+                      child: const Text(
+                        'Decline',
+                        style: TextStyle(color: Colors.red),
+                      )),
+                  TextButton(
+                      onPressed: (() {
+                        setState(() {
+                          followerRequests--;
+                        });
+                      }),
+                      child: const Text(
+                        'Accept',
+                        style: TextStyle(),
+                      ))
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatefulWidget {
+  int itemQuantity;
+  SecondPage({super.key, required this.itemQuantity});
+
+  @override
+  State<SecondPage> createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Basket'),
+        backgroundColor: Colors.orange,
+      ),
+      body: SafeArea(
+          child: Center(
+        child: Column(
+          children: [
+            basketCount > 0
+                ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.network(
+                          'https://cf.shopee.ph/file/4e9b29f378bdeb72bf313429e2d2223d'),
+                      Text('${widget.itemQuantity}')
+                    ],
+                  )
+                : const Text(''),
+          ],
+        ),
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            basketCount = 0;
+            widget.itemQuantity = 0;
+          });
+        },
+        backgroundColor: Colors.red,
+        child: const Icon(Icons.remove),
+      ),
     );
   }
 }
