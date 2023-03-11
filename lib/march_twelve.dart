addc6b1081ebd9bd4df651c436ddbcbaa9dd73ef
christiandarvs <christiandarvin4@gmail.com>
2023-03-12 00:58:42 +0800

    add march12 file

---
+++ lib/march_twelve.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class March12 extends StatelessWidget {
+  const March12({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        children: const [],
+      )),
+    );
+  }
+}
