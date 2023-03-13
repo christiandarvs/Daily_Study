a5634a20976edd3d1b2aeb2c76279b573fefc6c5
christiandarvs <christiandarvin4@gmail.com>
2023-03-13 22:48:49 +0800

    add march13

---
+++ lib/march_thirteen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class March13 extends StatelessWidget {
+  const March13({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        children: const [],
+      )),
+    );
+  }
+}
